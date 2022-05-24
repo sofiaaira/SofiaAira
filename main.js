fdd8c1a04dcb5cc8da2734c4bd486a41e1f5f5ef
Sofia <sofita.ap30@gmail.com>
2022-05-24 14:59:53 -0300

    hola

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("soy externo")
+// para acceder a un ID
+let parrafo1 = documente.getElementById("pConId")
+console.log(parrafo1)
+
+//para acceder a una clase
+let parrafo2 = document.getElementsByClassName("pConClass")
+console.log(parrafo2)
+
+// para acceder a una etiqueta
+let parrafo3 = document.getElementsByTagName("p")
+console.log(parrafo3)
+
+// cambiar texto con un boton
+function cambiarTitular(){
+let titular1 = document.getElementById("titular")
+
+titular1.innerText = "Hola cosa"
+}
