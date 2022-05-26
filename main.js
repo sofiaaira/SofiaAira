cccab1a1d8f45e87804943676bab30fd730d5d69
Sofia <sofita.ap30@gmail.com>
2022-05-25 23:25:57 -0300

    cambios

--- main.js
+++ main.js
@@ -2,25 +2,25 @@ console.log("soy externo")
 // para acceder a un ID
 
 
-let parrafo1 = document.getElementById("pConId")
-console.log(parrafo1)
+//let parrafo1 = document.getElementById("pConId")
+//console.log(parrafo1)
 
 //para acceder a una clase
 
-let parrafo2 = document.getElementsByClassName("pConClass")
-console.log(parrafo2)
+//let parrafo2 = document.getElementsByClassName("pConClass")
+//console.log(parrafo2)
 
 // para acceder a una etiqueta
 
-let parrafo3 = document.getElementsByTagName("p")
-console.log(parrafo3)
+//let parrafo3 = document.getElementsByTagName("p")
+//console.log(parrafo3)
 
 // cambiar texto con un boton
-function cambiarTitular(){
-let titular1 = document.getElementById("titular")
+//function cambiarTitular(){
+//let titular1 = document.getElementById("titular")
 
-titular1.innerText = "Hola cosa"
-}
+//titular1.innerText = "Hola cosa"
+//}
 
  
 
